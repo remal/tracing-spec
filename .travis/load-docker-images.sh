e8eff86cc55c160afedaea8260f12969b8d7d797
Semyon Levin <levin.semen@gmail.com>
2020-07-10 03:22:37 +0300

    CI

--- .travis/load-docker-images.sh
+++ .travis/load-docker-images.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 set -e
 
-if [ ! -d $HOME/.docker-images ]; then
+if [ ! -d "$HOME/.docker-images" ]; then
     exit 0
 fi
 
-ls $HOME/.docker-images/*.tar.gz | while read FILE; do
-    sem --will-cite --id docker-load -j 8 "zcat $FILE | docker load; echo Loaded $FILE"
+find "$HOME/.docker-images" -name '*.tar.gz' -type f | while read -r FILE; do
+    sem --will-cite --id docker-load -j 8 "zcat '$FILE' | docker load; echo '    from $FILE'"
 done
 
 sem --will-cite --id docker-load --wait
--- .travis/save-docker-images.sh
+++ .travis/save-docker-images.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 set -e
 
-mkdir -p $HOME/.docker-images
+mkdir -p "$HOME/.docker-images"
 
-docker images -a --filter='dangling=false' --format '{{.Repository}}:{{.Tag}}' | while read IMAGE; do
-    ESCAPED_IMAGE=`echo $IMAGE | sed 's#/#:#g'`
+docker images -a --filter='dangling=false' --format '{{.Repository}}:{{.Tag}}' | while read -r IMAGE; do
+    ESCAPED_IMAGE=$(echo "$IMAGE" | sed 's#/#:#g')
     FILE=$HOME/.docker-images/$ESCAPED_IMAGE.tar.gz
-    sem --will-cite --id docker-save -j 8 "docker save $IMAGE | gzip -9 > $FILE; echo Saved $IMAGE to $FILE"
+    sem --will-cite --id docker-save -j 8 "docker save '$IMAGE' | gzip -9 > '$FILE'; echo 'Saved $IMAGE to $FILE'"
 done
 
 sem --will-cite --id docker-save --wait
