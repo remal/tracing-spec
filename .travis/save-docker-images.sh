260f0559430036fda7a336219ff8a42ffdd142a1
Semyon Levin <levin.semen@gmail.com>
2020-07-13 16:18:26 +0300

    CI

--- .travis/save-docker-images.sh
+++ .travis/save-docker-images.sh
@@ -8,8 +8,9 @@ mkdir -p "$DIR"
 FILE="$DIR/images.tar"
 rm -rf "$FILE"
 
-IMAGES=(`docker images -a --filter='dangling=false' --format '{{.Repository}}:{{.Tag}}' | sort`)
-if [ ! -z "$IMAGES" ]; then
-    echo docker save ${IMAGES[@]} \> "$FILE"
-    docker save ${IMAGES[@]} > "$FILE"
+# shellcheck disable=SC2207
+IMAGES=($(docker images -a --filter='dangling=false' --format '{{.Repository}}:{{.Tag}}' | sort))
+if [ ${#IMAGES[@]} -ge 1 ]; then
+    echo docker save "${IMAGES[@]}" \> "$FILE"
+    docker save "${IMAGES[@]}" > "$FILE"
 fi
