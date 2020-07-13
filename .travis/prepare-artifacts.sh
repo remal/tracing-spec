96a7c7ae82affc21ad43e9955a28ae178341918a
Semyon Levin <levin.semen@gmail.com>
2020-07-13 22:40:01 +0300

    CI

---
+++ .travis/prepare-artifacts.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e +o pipefail
+
+ARTIFACTS_FILE=./.artifacts.zip
+
+# shellcheck disable=SC2207
+FILES=($(find . -name 'build' -type d | sort))
+if [ ${#FILES[@]} -ge 1 ]; then
+    echo zip -r -9 "$ARTIFACTS_FILE" "${FILES[@]}"
+    zip -r -9 "$ARTIFACTS_FILE" "${FILES[@]}" > /dev/nul
+fi
