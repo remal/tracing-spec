8d64aeae92516802e5271f8ad3aa9ee0a2227eda
Semyon Levin <levin.semen@gmail.com>
2020-07-13 20:51:16 +0300

    CI

---
+++ .travis/prepare-artifacts.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e +o pipefail
+
+ARTIFACTS_DIR=./.artifacts
+mkdir -p "$ARTIFACTS_DIR"
+
+while read -r DIR; do
+    DIR_RELATIVE_PATH=${DIR:2}
+
+    PARENT=$(dirname "$DIR")
+    PARENT_RELATIVE_PATH=${PARENT:2}
+    PARENT_DIR="$ARTIFACTS_DIR/$PARENT_RELATIVE_PATH"
+    mkdir -p "$PARENT_DIR"
+
+    REAL_DIR=$(realpath "$DIR")
+    REAL_PARENT_DIR=$(realpath "$PARENT_DIR")
+    echo ln -s "$REAL_DIR" "$REAL_PARENT_DIR"
+    ln -s "$REAL_DIR" "$REAL_PARENT_DIR"
+done < <(find . -name 'docker' -type d)
