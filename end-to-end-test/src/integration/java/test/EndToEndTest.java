75ab255465be8c72077f9f05f5e87a169d900322
Semyon Levin <levin.semen@gmail.com>
2020-08-30 23:53:30 +0300

    end-to-end test debug

--- end-to-end-test/src/integration/java/test/EndToEndTest.java
+++ end-to-end-test/src/integration/java/test/EndToEndTest.java
@@ -17,6 +17,7 @@
 package test;
 
 import static java.util.Arrays.asList;
+import static name.remal.tracingspec.model.SpecSpansGraphs.createSpecSpansGraph;
 import static org.awaitility.Awaitility.await;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
@@ -30,12 +31,15 @@ import apps.schemas.SchemasApplication;
 import apps.schemas.SchemasClient;
 import apps.users.UsersApplication;
 import brave.Tracer;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.val;
 import name.remal.tracingspec.retriever.jaeger.JaegerSpecSpansRetriever;
 import name.remal.tracingspec.retriever.jaeger.JaegerSpecSpansRetrieverProperties;
+import name.remal.tracingspec.retriever.zipkin.ZipkinSpecSpansRetriever;
+import name.remal.tracingspec.retriever.zipkin.ZipkinSpecSpansRetrieverProperties;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterAll;
@@ -47,13 +51,14 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import shared.SharedConfiguration;
 import utils.test.container.JaegerAllInOneContainer;
+import utils.test.container.ZipkinContainer;
 
 class EndToEndTest {
 
     private static final Logger logger = LogManager.getLogger(EndToEndTest.class);
 
     @Test
-    void test() {
+    void test() throws Throwable {
         val schema = ImmutableSchema.builder()
             .id("task")
             .addReference(ImmutableSchemaReference.builder()
@@ -85,18 +90,34 @@ class EndToEndTest {
             docs -> docs.stream().noneMatch(oldSchemaDocuments::contains)
         );
 
-        val newDocuments = documentsClient.getAllDocumentsBySchema(schema.getId());
-        assertThat(newDocuments, not(empty()));
+        val newSchemaDocuments = documentsClient.getAllDocumentsBySchema(schema.getId());
+        assertThat(newSchemaDocuments, not(empty()));
         logger.info("OLD DOCUMENTS: {}", oldSchemaDocuments);
-        logger.info("NEW DOCUMENTS: {}", newDocuments);
+        logger.info("NEW DOCUMENTS: {}", newSchemaDocuments);
+
+        val objectMapper = applicationContexts.values().iterator().next().getBean(ObjectMapper.class);
 
         {
             val retrieverProperties = new JaegerSpecSpansRetrieverProperties();
             retrieverProperties.setHost("localhost");
             retrieverProperties.setPort(sharedContext.getBean(JaegerAllInOneContainer.class).getQueryPort());
-            val jaegerRetriever = new JaegerSpecSpansRetriever(retrieverProperties);
-            val specSpans = jaegerRetriever.retrieveSpecSpansForTrace(testSpan.context().traceIdString());
-            logger.info("Jaeger spec spans: {}", specSpans);
+            val retriever = new JaegerSpecSpansRetriever(retrieverProperties);
+            val specSpans = retriever.retrieveSpecSpansForTrace(testSpan.context().traceIdString());
+            val graph = createSpecSpansGraph(specSpans);
+            logger.info("Jaeger graph: {}", graph);
+            val graphJson = objectMapper.writeValueAsString(graph);
+            logger.info("Jaeger graph JSON: {}", graphJson);
+        }
+
+        {
+            val retrieverProperties = new ZipkinSpecSpansRetrieverProperties();
+            retrieverProperties.setUrl(sharedContext.getBean(ZipkinContainer.class).getZipkinBaseUrl());
+            val retriever = new ZipkinSpecSpansRetriever(retrieverProperties);
+            val specSpans = retriever.retrieveSpecSpansForTrace(testSpan.context().traceIdString());
+            val graph = createSpecSpansGraph(specSpans);
+            logger.info("Zipkin graph: {}", graph);
+            val graphJson = objectMapper.writeValueAsString(graph);
+            logger.info("Zipkin graph JSON: {}", graphJson);
         }
     }
 
