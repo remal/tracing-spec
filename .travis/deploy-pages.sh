fb1a768eb79fe50935ae6186fd4cfb2fefbef317
Semyon Levin <levin.semen@gmail.com>
2020-09-22 22:06:22 +0300

    test

--- .travis/deploy-pages.sh
+++ .travis/deploy-pages.sh
@@ -13,11 +13,12 @@ PROJECT_SLUG=tracing-spec
 PAGES_ROOT_DIR=./.gh-pages
 PROJECT_PAGES_DIR="${PAGES_ROOT_DIR:?}/${PROJECT_SLUG:?}"
 
-echo "Cloning 'https://github.com/remal/remal.github.io.git' to '$PAGES_ROOT_DIR'..."
+echo "Cloning 'https://github.com/remal/remal.github.io.git'..."
 git clone "https://${GITHUB_TOKEN}@github.com/remal/remal.github.io.git" "$PAGES_ROOT_DIR"
 
 #git -C "$PAGES_ROOT_DIR" config --global user.email "travis@travis-ci.org"
 #git -C "$PAGES_ROOT_DIR" config --global user.name "Travis CI"
+ls
 
 echo "Updating content"
 rm -rf "${PROJECT_PAGES_DIR:?}"
@@ -31,5 +32,5 @@ if git -C "$PAGES_ROOT_DIR" diff-index --quiet HEAD; then
     git -C "$PAGES_ROOT_DIR" commit --all -m "Update pages for $PROJECT_SLUG"
     git -C "$PAGES_ROOT_DIR" push
 else
-    echo "Nothing changes"
+    echo "Nothing changed"
 fi
