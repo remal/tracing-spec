682d5a51751930f2f9f6c2c9d9784e5afe21e927
Semyon Levin <levin.semen@gmail.com>
2020-10-05 11:37:19 +0300

    CI

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,6 +8,10 @@ export DISABLE_JAR_TASKS=true
 #./retry ./gradlew releaseNexusRepositories
 
 RELEASE_ASSET=./build/tracing-spec-app.jar
+RELEASE_ASSET_DIR=$(dirname $RELEASE_ASSET)
+RELEASE_ASSET_NAME=$(basename $RELEASE_ASSET)
+mkdir -p "$RELEASE_ASSET_DIR"
+cp ./tracing-spec-application/build/libs/tracing-spec-application-*-fatjar.jar "$RELEASE_ASSET"
 gh release upload "$TRAVIS_TAG" "$RELEASE_ASSET"
 
 ./.travis/deploy-pages.sh
