f62b17363dc1120adf81e54a7932ad656f9d2673
Semyon Levin <levin.semen@gmail.com>
2020-11-28 22:36:20 +0300

    JaegerIdUtils fix

--- tracing-spec-retriever-jaeger/src/main/java/name/remal/tracingspec/retriever/jaeger/JaegerIdUtils.java
+++ tracing-spec-retriever-jaeger/src/main/java/name/remal/tracingspec/retriever/jaeger/JaegerIdUtils.java
@@ -26,10 +26,22 @@ import org.jetbrains.annotations.ApiStatus.Internal;
 
 @Internal
 @SuppressWarnings("java:S109")
-interface JaegerIdUtils {
+abstract class JaegerIdUtils {
+
+    private static final int BYTES_LENGTH_DIVIDER = 8;
+    private static final int CHARS_LENGTH_DIVIDER = BYTES_LENGTH_DIVIDER * 2;
+    private static final String ZERO_CHARS;
+
+    static {
+        val sb = new StringBuilder(CHARS_LENGTH_DIVIDER);
+        for (int i = 0; i < CHARS_LENGTH_DIVIDER; ++i) {
+            sb.append('0');
+        }
+        ZERO_CHARS = sb.toString();
+    }
 
     @SneakyThrows
-    static byte[] encodeJaegerId(String jaegerStringId) {
+    public static byte[] encodeJaegerId(String jaegerStringId) {
         byte[] bytes;
         if ((jaegerStringId.length() % 2) == 0) {
             bytes = decodeHex(jaegerStringId);
@@ -37,8 +49,8 @@ interface JaegerIdUtils {
             bytes = decodeHex('0' + jaegerStringId);
         }
 
-        if ((bytes.length % 8) != 0) {
-            val newLength = ((bytes.length / 8) + 1) * 8;
+        if ((bytes.length % BYTES_LENGTH_DIVIDER) != 0) {
+            val newLength = ((bytes.length / BYTES_LENGTH_DIVIDER) + 1) * BYTES_LENGTH_DIVIDER;
             val newBytes = new byte[newLength];
             arraycopy(bytes, 0, newBytes, newLength - bytes.length, bytes.length);
             bytes = newBytes;
@@ -48,18 +60,19 @@ interface JaegerIdUtils {
     }
 
     @SneakyThrows
-    static String decodeJaegerId(byte[] jaegerId) {
+    public static String decodeJaegerId(byte[] jaegerId) {
         String string = encodeHexString(jaegerId, true);
 
-        int notZeroCharIndex = 0;
-        while (notZeroCharIndex < string.length() - 1 && string.charAt(notZeroCharIndex) == '0') {
-            ++notZeroCharIndex;
-        }
-        if (notZeroCharIndex > 0) {
-            string = string.substring(notZeroCharIndex);
+        if ((string.length() % CHARS_LENGTH_DIVIDER) != 0) {
+            val newLength = ((string.length() / CHARS_LENGTH_DIVIDER) + 1) * CHARS_LENGTH_DIVIDER;
+            string = ZERO_CHARS.substring(0, newLength - string.length()) + string;
         }
 
         return string;
+    }
+
+
+    private JaegerIdUtils() {
     }
 
 }
